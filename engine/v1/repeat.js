302d2cdb8023a6aa7d66c7e7d6fb4e6b5273e76b
Dmitrii Boikov <boikovdv@gmail.com>
2022-07-15 15:15:10 +0300

    fix dom-changed event dispatch on repeater change clones

--- engine/v1/repeat.js
+++ engine/v1/repeat.js
@@ -55,7 +55,7 @@ class Repeater extends PropertiesMixin(ContextMixin(EventTarget)) {
                     let sibling = this.clones[mutation.index]?._ti._nodes[0];
                     let clones = this.renderItems(mutation.added, sibling, mutation.index);
                     this.clones.splice(mutation.index, 0, ...clones);
-                    this.dispatchEvent(new CustomEvent('dom-changed', { bubbles: true, composed: true }));
+                    this.anchor.dispatchEvent(new CustomEvent('dom-changed', { bubbles: true, composed: true }));
                     break;
                 }
             // noinspection FallThroughInSwitchStatementJS
@@ -83,6 +83,7 @@ class Repeater extends PropertiesMixin(ContextMixin(EventTarget)) {
                         }
                         if ( items.length ) this.clones.push(...this.renderItems(items, this.anchor));
                     }
+                    this.anchor.dispatchEvent(new CustomEvent('dom-changed', { bubbles: true, composed: true }));
                 }
                 break;
         }
